98a8139beb4638391e388b4cbdae4c6e3079adba
Severi Tikkanen <severitikkanen@gmail.com>
2019-10-28 13:32:12 +0200

    Added client with deps and concurrently

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <div className="App" style={{ margin: 16 }}>
+        App
+      </div>
+    </>
+  );
+}
+
+export default App;

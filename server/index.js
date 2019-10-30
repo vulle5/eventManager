493607c846054dc009942fae79333b18a759c2c9
Severi Tikkanen <severitikkanen@gmail.com>
2019-10-30 22:28:16 +0200

    Removed some old code

--- server/index.js
+++ server/index.js
@@ -2,18 +2,6 @@ require('dotenv').config();
 const app = require('./app');
 const http = require('http');
 
-// MONGODB_URI=osoite_tahan npm run watch
-// Give this to Taikala for db access
-
-/*
-  if ( process.argv.length<3 ) {
-  console.log('give password as argument')
-  process.exit(1)
-  }
-
-  const password = process.argv[2]
-*/
-
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3003;

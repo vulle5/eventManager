87278374ce3029370e30516106c99eb8204c6ad6
Severi Tikkanen <severitikkanen@gmail.com>
2019-10-30 16:39:11 +0200

    Now you can see participants for event

--- client/src/components/EventView/EventView.js
+++ client/src/components/EventView/EventView.js
@@ -4,6 +4,7 @@ import {
   Paper,
   Typography,
   Avatar,
+  List,
   ListItem,
   ListItemText,
   ListItemAvatar,
@@ -125,6 +126,21 @@ function EventView({ match: { params }, token, username }) {
             href={`http://${event.location.webUrl}`}
           >{`web: ${event.location.webUrl}`}</a>
         </Typography>
+        <Typography variant="h5" style={{ marginBottom: 16 }}>
+          Osallistujat
+        </Typography>
+        <List style={{ display: 'flex', flexWrap: 'wrap', marginBottom: 32 }}>
+          {event.participants
+            .filter(({ type }) => type === 'yes')
+            .map(({ participant }) => (
+              <ListItem key={participant.id} style={{ maxWidth: 250 }}>
+                <ListItemAvatar>
+                  <Avatar>{participant.name.substring(0, 1)}</Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={participant.name} />
+              </ListItem>
+            ))}
+        </List>
         <Typography variant="h5" style={{ marginBottom: 16 }}>
           Haluan Osallistua...
         </Typography>
